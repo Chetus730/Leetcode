8d880d3c7120b10d5e1544900ba101c60f8d159c
Chetan Chaudhari <chetanchaudhari4776@gmail.com>
2026-09-05 23:16:35 +0530

    Time: 295 ms (64.5%), Space: 67.9 MB (81.7%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    activity.sort_values(['player_id', 'event_date'], inplace=True)
+    activity.drop_duplicates(subset=['player_id'], keep='first', inplace=True)
+    return activity[['player_id', 'event_date']].rename(
+        columns={'event_date': 'first_login'}
+    )
